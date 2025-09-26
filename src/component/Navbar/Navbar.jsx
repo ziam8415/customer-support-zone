1f38363e65bb2e327382a8112db9b61859ed6bff
ziam123-hub <ziamakhand@gmail.com>
2025-09-26 23:17:12 +0600

    added navber desing

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -2,26 +2,21 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    // <div className="flex justify-between items-center">
-    //   <h1></h1>
-    //   <div className="flex justify-between items-center gap-4 flex-wrap">
-
-    //
-    //   </div>
-    // </div>
-
-    <div className="navbar bg-white flex flex-col md:flex-row ">
-      <div className="flex-1 ">
-        <a className="btn btn-ghost text-xl">CS — Ticket System</a>
+    <div className="navbar bg-white flex justify-between flex-col md:flex-row ">
+      <div className=" ">
+        <a className=" text-2xl font-bold">CS — Ticket System</a>
       </div>
-      <div className="flex flex-1 justify-center items-center gap-4 flex-wrap ">
+      <div className="flex  justify-center items-center gap-4 text-gray-700 flex-wrap ">
         <p>Home</p>
         <p>FAQ</p>
         <p>Changelog</p>
         <p>Blog</p>
         <p>Download</p>
         <p>Contact</p>
-        <button className="btn"> + New Ticket</button>
+        <button className="bg-gradient-to-r from-[#632ee3] to-[#9c62f2] px-4 py-2 rounded-2xl text-white text-xl font-semibold">
+          {" "}
+          + New Ticket
+        </button>
       </div>
     </div>
   );
